488782da789693a5289284ab752deffac1c0cd6c
Steve <stevemolele@yahoo.com>
2021-06-19 05:47:56 +0200

    halfway through

---
+++ task-0.5.js
@@ -0,0 +1,11 @@
+function areaOfTriangle(num1, num2, num3) {
+    // Used Heron's formula
+    let sides = (num1 + num2 + num3) / 2;
+
+    let area = Math.sqrt(sides * ((sides - num1) * (sides - num2) * (sides - num3)));
+
+    console.log(area);
+};
+
+areaOfTriangle(3, 4, 5);
+// Formula only works if we know the values of all the sides
