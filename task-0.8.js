713b75935923bc8159c406c35bec147f622d34f9
Steve <stevemolele@yahoo.com>
2021-06-29 00:22:41 +0200

    resolved mistakes

--- task-0.8.js
+++ task-0.8.js
@@ -2,12 +2,16 @@ function convertNumToTime(num) {
     let hours = Math.floor(num / 60);
     let minutes = num % 60;
 
-    if (hours <= 1 && minutes <= 1) {
-        console.log(hours + " hours, " + minutes + " minutes.");
+    if (hours == 1 && minutes <= 1) {
+        console.log(hours + " hour, " + minutes + " minute.");
     } else if (hours > 1 && minutes == 1) {
         console.log(hours + " hours, " + minutes + " minute.");
-    } else if (hours <= 1 && minutes > 1) {
+    } else if (hours == 1 && minutes > 1) {
         console.log(hours + " hour, " + minutes + " minutes.");
+    } else if (hours == 1 && minutes == 1) {
+        console.log(hours + " hour, " + minutes + " minute.");
+    } else if (hours < 1 && minutes == 1) {
+        console.log(hours + " hours, " + minutes + " minute.");
     } else {
         console.log(hours + " hours, " + minutes + " minutes.");
     }
